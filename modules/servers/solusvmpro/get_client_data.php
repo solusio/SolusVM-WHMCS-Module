859cbf474c984abc157f50fa841b6756c89675ff
Alexandr Redikultsev <aredikultsev@plesk.com>
2020-10-27 14:42:34 +0700

    Adding different way of finding client ID for WHMCS 8.x

--- modules/servers/solusvmpro/get_client_data.php
+++ modules/servers/solusvmpro/get_client_data.php
@@ -8,6 +8,7 @@ require_once __DIR__ . '/lib/Curl.php';
 require_once __DIR__ . '/lib/CaseInsensitiveArray.php';
 require_once __DIR__ . '/lib/SolusVM.php';
 
+use Composer\Semver\Comparator;
 use Illuminate\Database\Capsule\Manager as Capsule;
 use SolusVM\SolusVM;
 
@@ -21,7 +22,12 @@ if ( ! $ca->isLoggedIn() ) {
     exit();
 }
 
+// Adding this to workaround CORE-15619 WHMCS bug
+$whmcsVersion = $CONFIG['Version'];
 $uid = $ca->getUserID();
+if (Comparator::greaterThanOrEqualTo($whmcsVersion,'8')) {
+    $uid = Auth::client()->id;
+}
 
 $params = SolusVM::getParamsFromVserviceID( $vserverid, $uid );
 if ( ! $params ) {
