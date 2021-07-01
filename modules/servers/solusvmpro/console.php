4df401aa1c1576331530c5ab5674ada1647a42b4
Alexandr Redikultsev <aredikultsev@plesk.com>
2021-07-01 10:59:32 +0700

    Replacing __DIR__ with ROOTDIR for WHMCS 8 compatibility

--- modules/servers/solusvmpro/console.php
+++ modules/servers/solusvmpro/console.php
@@ -3,9 +3,9 @@
 define( "CLIENTAREA", true );
 require( "../../../init.php" );
 
-require_once __DIR__ . '/lib/Curl.php';
-require_once __DIR__ . '/lib/CaseInsensitiveArray.php';
-require_once __DIR__ . '/lib/SolusVM.php';
+require_once ROOTDIR . '/modules/servers/solusvmpro/lib/Curl.php';
+require_once ROOTDIR . '/modules/servers/solusvmpro/lib/CaseInsensitiveArray.php';
+require_once ROOTDIR . '/modules/servers/solusvmpro/lib/SolusVM.php';
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 use SolusVM\SolusVM;
